b9c3625cecce6db3ced7dd295434145a465d72ce
Lucas Davila <lucasdavila100@gmail.com>
2025-04-08 20:09:34 -0300

    App.js

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+
+// Importa os componentes principais da aplicação
 import Title from './src/components/Title';
 import FormIMC from './src/components/FormIMC';
 
 export default function App() {
-  return (
+  return (    
+    // View principal que agrupa os componentes na tela
     <View style={styles.container}>
       <Title />
       <FormIMC />
     </View>
   );
 }
-
+// Estilo da tela principal
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 8,
-    justifyContent: 'center',
+    flex: 1,  // Ocupa toda a tela
+    backgroundColor: '#fff', // Fundo branco
+    padding: 8, // Espaçamento interno
+    justifyContent: 'center', // Centraliza verticalmente
   },
 });
