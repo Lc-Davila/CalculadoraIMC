157c2d04168423b14f78f7cab740bcda8f3a3e01
Lucas Davila <lucasdavila100@gmail.com>
2025-04-08 19:51:53 -0300

    Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,21 +3,23 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function Title() {
   return (
+     // Caixa que contém o título
     <View style={styles.boxTitle}>
       <Text style={styles.textTitle}>Calculadora de IMC</Text>
     </View>
   );
 }
+// Estilos do componente Title
 
 const styles = StyleSheet.create({
   boxTitle: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
+    alignItems: 'center',  // centraliza horizontalmente
+    justifyContent: 'center', // Centraliza verticalmente
+    padding: 10, // Espaçamento interno
   },
   textTitle: {
-    color: '#000',
-    fontSize: 24,
-    fontWeight: 'bold',
+    color: '#000', // Cor preta
+    fontSize: 24, // Tamanho da fonte
+    fontWeight: 'bold', //  Deixa em negrito
   },
 });
